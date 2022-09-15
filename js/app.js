9108ece37d53ad449483daf14e6307775d172890
Alexandre Delfino <alex.delf@laposte.net>
2022-09-15 17:05:09 +0200

    prevent default

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded", e => {
+  e.preventDefault();
+  let g_mainfile = document.getElementById("mainfile");
+  let g_explo = document.getElementById("fichieropen");
   //power on
   document.querySelector("header h1 button").addEventListener("click", () => {
     document.getElementById("poweron").style.display = "none";
   });
+  document.querySelector("header h1 button").addEventListener("keydown", () => {
+    document.getElementById("poweron").style.display = "none";
+  });
   //open file explo  
-  document.getElementById("mainfile").addEventListener("click", () => {
-    document.getElementById("fichieropen").style.transform = "scale(1)";
+  g_mainfile.addEventListener("click", () => {
+    g_explo.style.transform = "scale(1)";
   });
   //close file explo
-  document.getElementById("close").addEventListener("click", () => {
-    document.getElementById("fichieropen").style.transform = "scale(0)";
+  g_mainfile.addEventListener("click", () => {
+    g_explo.style.transform = "scale(0)";
   });
 });
