57c458623a2c2211e8dce80484b478d160b7bf6d
Alexandre Delfino <alex.delf@laposte.net>
2022-09-20 13:54:26 +0200

    file explo avance

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
 document.addEventListener("DOMContentLoaded", e => {
-  e.preventDefault();
-  let g_mainfile = document.getElementById("mainfile");
-  let g_explo = document.getElementById("fichieropen");
+
+  const g_mainfile = document.getElementById("mainfile");
+  const g_explo = document.getElementById("fichieropen");
+  const b_close = document.getElementById("close");
+  const a_chemin = document.getElementById("chemin");
   //power on
   document.querySelector("header h1 button").addEventListener("click", () => {
     document.getElementById("poweron").style.display = "none";
   });
-  document.querySelector("header h1 button").addEventListener("keydown", () => {
-    document.getElementById("poweron").style.display = "none";
-  });
+
+  // document.querySelector("header h1 button").addEventListener("keydown", () => {
+  //   document.getElementById("poweron").style.display = "none";
+  //   });
   //open file explo  
   g_mainfile.addEventListener("click", () => {
     g_explo.style.transform = "scale(1)";
+    a_chemin.append("Portfolio_A_Delfino/");
   });
   //close file explo
-  g_mainfile.addEventListener("click", () => {
+  b_close.addEventListener("click", () => {
     g_explo.style.transform = "scale(0)";
+    a_chemin.innerHTML -= "Portfolio_A_Delfino/";
   });
 });
